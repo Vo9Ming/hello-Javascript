bfc856648fb8b96837d1d5cfa3cfb0678153a1b1
Vo9Ming <kimminj2017@gmail.com>
2022-09-20 12:53:10 +0900

    DOM과 HTML
    실습예제1 js파일

---
+++ step17_dom_test.js
@@ -0,0 +1,17 @@
+// var msg = "Hello";
+// alert(msg);
+
+//////////////////////////////////////////////////
+
+var list = document.getElementsByTagName("h1");
+list[0].innerHTML;
+list[0].innerHTML = "Hello";
+// 첫번째 h1태그를 가져와서 첫번째 h1태그에 담긴 내용을 Hello로 변경한다.
+
+/////////////////////////////////////////////////
+
+var list = document.getElementById("main");
+main.innerHTML = "Hello";
+// 'main' 이름을 가진 Id요소를 가져와서 그 요소에 Hello를 담는다.
+
+/////////////////////////////////////////////////
